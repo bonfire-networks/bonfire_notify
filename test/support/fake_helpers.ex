df86e77deec44c6a97593160ea7b273e7cb6fcb1
Mayel <git@mayel.space>
2021-03-13 15:21:32 +0100

    names

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -2,7 +2,7 @@ defmodule Bonfire.Notify.Test.FakeHelpers do
 
   alias Bonfire.Data.Identity.Account
   alias Bonfire.Me.Fake
-  alias Bonfire.Me.Identity.{Accounts, Users}
+  alias Bonfire.Me.{Accounts, Users}
 
   import ExUnit.Assertions
 
