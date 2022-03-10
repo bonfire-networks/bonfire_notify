19cbfee412c3f46bc829332c06b5fcc3ad4dc189
Mayel de Borniol <git@mayel.space>
2022-03-10 18:28:34 +1300

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -8,22 +8,6 @@ defmodule Bonfire.Notify.Test.FakeHelpers do
 
   import Bonfire.Common.Config, only: [repo: 0]
 
-  def fake_account!(attrs \\ %{}) do
-    cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = repo().insert(cs)
-    account
-  end
-
-  def fake_user!(account \\ %{}, attrs \\ %{})
-
-  def fake_user!(%Account{}=account, attrs) do
-    assert {:ok, user} = Users.create(Fake.user(attrs), account)
-    user
-  end
-
-  def fake_user!(account_attrs, user_attrs) do
-    fake_user!(fake_account!(account_attrs), user_attrs)
-  end
 
   def valid_push_subscription_data(endpoint \\ "https://endpoint.test") do
     """
