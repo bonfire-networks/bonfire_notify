b39581f55461121ab1eef2f20f9f8856c61504a2
Mayel de Borniol <git@mayel.space>
2022-04-22 08:09:58 +1200

    c

--- mix.exs
+++ mix.exs
@@ -21,12 +21,7 @@ defmodule Bonfire.Notify.MixProject do
     ]
   end
 
-  def application do
-    [
-      mod: {Bonfire.Notify, []},
-      extra_applications: [:logger, :runtime_tools]
-    ]
-  end
+  def application, do: [ extra_applications: [:logger, :runtime_tools] ]
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
