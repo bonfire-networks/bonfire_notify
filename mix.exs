a90c118de21f1cf2004acd7aba6296a77d3a2c30
Mayel de Borniol <git@mayel.space>
2022-05-03 21:29:15 +1200

    mox

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Bonfire.Notify.MixProject do
         {:phoenix_live_reload, "~> 1.2", only: :dev},
         {:dbg, "~> 1.0", only: [:dev, :test]},
         {:floki, ">= 0.0.0", only: [:dev, :test]},
-        {:mox, "~> 0.4.0", only: [:dev, :test]},
+        {:mox, "~> 0.5", only: [:dev, :test]},
       ]
     ]
   end
