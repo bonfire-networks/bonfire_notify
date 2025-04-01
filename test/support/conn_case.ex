bbb94c81b67bec8f3e5958e3f5b52a4d7f0afda8
Mayel de Borniol <git@mayel.space>
2025-04-01 12:46:17 +0100

    v

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -36,6 +36,8 @@ defmodule Bonfire.Notify.ConnCase do
 
       # The default endpoint for testing
       @endpoint Application.compile_env!(:bonfire, :endpoint_module)
+
+      @moduletag :ui
     end
   end
 
